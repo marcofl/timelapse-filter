b52730225d3caf0631f66de82aa687146b13f3c6
fre Puppet-managed User <fre@GREER.uniq.li>
2015-08-14 13:03:13 +0200

    much better, direct output to ffmpeg on stdout. from end date or weeknumber filter

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # timelapse-filter
 * php script hack for filtering jpeg photos for timelapse videos
-* currently just filters for sunrise / sunset
+* currently filters for sunrise / sunset
+* accepts start end date or week of year
 * hardly depends on how photo files are named!
 
 ## example
- php clean_images.php | ./read.sh | ffmpeg -f image2pipe -r 25 -vcodec mjpeg -probesize 10M -i - -r 25 -q:v 2 -pix_fmt yuv420p -s hd480 /var/www/html/test.mp4
+ ./jpg_filter.php /pool/images/camsev1 $(date +%Y%m%d) $(date +%Y%m%d) | ffmpeg -f image2pipe -r 9 -vcodec mjpeg -probesize 10M -i - -r 9 -q:v 2 -pix_fmt yuv420p -s hd480 -y /var/www/html/video/daily-$(date +%Y%m%d).mp4
 
 ## calendar
 * get the csv from http://galupki.de/kalender/sunmoon.php
 
 ## todo
-* read jpegs directly in the php script, so stdout can be used directly as ffmpeg image2pipe input
 * make it simpler and cleaner
-
+* maybe integrate ffmpeg somehow
 
 USE AT YOUR OWN RISK!
--- jpg_filter.php
+++ jpg_filter.php
@@ -2,24 +2,30 @@
 <?php
 
 $basedir=$argv[1];
-$start_date=$argv[2];
-$end_date=$argv[3];
 
+if(!isset($argv[2])) {
+    echo "Usage: ".$argv[0]." <path> <week_number|start_date> [end_date] | ffmpeg...\n";
+    exit(1);
+}
+
+// week number or start end date
+if(intval($argv[2]) <= 53) {
+    $week=intval($argv[2]);
+} else {
+    $start_date=$argv[2];
+    $end_date=$argv[3];
+}
 $sunmoon_file='sunmoon.csv';
 
 // functions
-function read_sun_rise_set($file,$date) {
-    $csv['Datum']=0;
-    $csv['SAofficial']=6;
-    $csv['SUofficial']=7;
-
+function read_cal($file,$date) {
     $f = fopen($file, "r");
     $result = false;
     while ($row = fgetcsv($f,0,";")) {
         if ($row[0] == $date) {
-            $values['date']=$row[$csv['Datum']];
-            $values['rise']=$row[$csv['SAofficial']];
-            $values['set']=$row[$csv['SUofficial']];
+            $values['sunrise']=$row[6]; // SAofficial
+            $values['sunset']=$row[7]; // SUofficial
+            $values['week']=$row[2]; // week
             return $values;
             break;
         }
@@ -44,17 +50,16 @@ foreach ($dirs as $dir) {
         
         $date=strtotime(explode("-",$file_date[1])[0]);
         $time=strtotime(explode("-",$file_date[1])[1]);
+
+        // get calendar values for this day
+        if ($date != $last_date) {
+          $cal=read_cal($sunmoon_file,date('j.n.Y', $date));
+        }
+        $last_date = $date;
         
-        if ($date >= strtotime($start_date) && $date <= strtotime($end_date)) {
-            
-            // get daylight hours
-            if ($date != $last_date) {
-              $daylight=read_sun_rise_set($sunmoon_file,date('j.n.Y', $date));
-            }
-            $last_date = $date;
-            
-            if ($time >= strtotime($daylight['rise']) && $time <= strtotime($daylight['set'])) {
-              //echo $basedir."/".$dir."/".$file."\n";
+        if (($date >= strtotime($start_date) && $date <= strtotime($end_date)) || $week == $cal['week']) {    
+            if ($time >= strtotime($cal['sunrise']) && $time <= strtotime($cal['sunset'])) {
+//              echo $basedir."/".$dir."/".$file."\n";
               readfile($basedir."/".$dir."/".$file);
             }
         }
